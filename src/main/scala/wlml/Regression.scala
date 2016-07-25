b3e16bfde2623ffd19c4a728a1464d26d5e28ec3
Woosang Lee <recollect12@gmail.com>
2016-07-24 21:12:27 -0400

    Changed DenseVector and DenseMatrix to SparseVector and CSCMatrix

--- src/main/scala/wlml/Regression.scala
+++ src/main/scala/wlml/Regression.scala
@@ -1,47 +1,47 @@
 package main.scala.wlml
 import breeze.linalg._
 
-class Regression(ys: Array[Double], xs: Array[Array[Double]]) {
-  private def features: DenseMatrix[Double] = {
-    DenseMatrix.horzcat(
-      DenseMatrix(List.fill(xs.length)(1.0): _*),
-      DenseMatrix(xs: _*))
+object Regressor {
+
+  case class Parameters(stepSize: Double, tolerance: Double, maxiter: Int)
+  
+  class Regression(ys: Array[Double], xs: Array[Array[Double]]) {
+    private def features: CSCMatrix[Double] = {
+      CSCMatrix(xs.map( row => 1.0 +: row ):_*)
+    }
+    private def outputs: SparseVector[Double] = SparseVector(ys)
+
+    def gradientdescent(weights: SparseVector[Double], features: CSCMatrix[Double],
+      output: SparseVector[Double], parameters: Parameters, iter: Int): SparseVector[Double] = {
+
+      def getCost(output: SparseVector[Double], weights: SparseVector[Double],
+        features: CSCMatrix[Double], param: Parameters): Tuple3[SparseVector[Double], Double, SparseVector[Double]] = {
+        val m: Double = features.rows
+        val errors = (features * weights) - output
+        val cost = (1.0 / m) * (errors.t * errors)
+        val gradient = (features.t * errors) * (m / 2.0)
+        weights :-= gradient * param.stepSize
+
+        (weights, cost, errors)
+      }
+
+      def checkNecessity(distance: Double, tol: Double, iter: Int, maxiter: Int): Boolean = {
+        if ((distance < tol) || (iter >= maxiter)) false
+        else true
+      }
+
+      //result._1 = weights
+      //result._2 = cost
+      //result._3 = errors
+      val (weightNext: SparseVector[Double], cost: Double, errors: SparseVector[Double]) = getCost(output, weights, features, parameters)
+
+      val relative_r = norm(errors) / norm(output)
+
+      if (checkNecessity(relative_r, parameters.tolerance, iter, parameters.maxiter)) {
+        gradientdescent(weightNext, features, output, parameters, iter + 1)
+      } else weightNext
+
+    }
+
   }
-  private def outputs: DenseVector[Double] = DenseVector(ys)
-
-  def gradientdescent(weights: DenseVector[Double], features: DenseMatrix[Double],
-    output: DenseVector[Double], step_size: Double, tol: Double, 
-    iter: Int, maxiter: Int):DenseVector[Double] = {
-
-    def getCost(output: DenseVector[Double], weights: DenseVector[Double],
-      features: DenseMatrix[Double], step_size: Double): Tuple3[DenseVector[Double], Double, DenseVector[Double]] = {
-      val m: Double = features.rows
-      val errors = (features * weights) - output
-      val cost = (1.0 / m) * (errors.t * errors)
-      val gradient = (features.t * errors) * (m / 2.0)
-      weights :-= gradient * step_size
-      
-      (weights, cost, errors)
-    }
-    
-    def checkNecessity(distance: Double, tol: Double, iter: Int, maxiter: Int): Boolean = {
-      if (distance > tol) true
-      else if (iter < maxiter) true
-      else false
-    }
-    
-    //result._1 = weights
-    //result._2 = cost
-    //result._3 = errors
-    val (weightNext: DenseVector[Double], cost:Double, errors: DenseVector[Double]) = getCost(output, weights, features, step_size)
-    
-    val relative_r = norm(errors) / norm(output)
-      
-    if (checkNecessity(relative_r, tol, iter, maxiter)) {
-      gradientdescent(weightNext, features, output, step_size, tol, iter+1, maxiter)
-    }
-    else weights,
-
-  }
-
 }
